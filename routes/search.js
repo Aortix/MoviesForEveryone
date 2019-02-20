d1ca4307f64ad94e49aff75a7b5087e1ce852bb9
Stephen <ridingjr@gmail.com>
2019-02-20 15:19:35 -0800

    created route for making api calls specifically for searching for movies. still adjusting it.

--- routes/search.js
+++ routes/search.js
@@ -1,19 +1,87 @@
 const express = require("express");
 const router = express.Router();
+const fetch = require("node-fetch");
+const genreIds = require("../movieGenres");
+
+const theMovieDbBaseUrl =
+  "https://api.themoviedb.org/3/search/movie?language=en-US";
 
 router.get("/", (req, res) => {
-  res.send({
-    type: "GET",
-    testing: "Yes"
-  });
-});
+  let count = 0;
 
-router.post("/addmovie", (req, res) => {
-  res.send({
-    type: "POST",
-    movie: req.body.movie,
-    genre: req.body.genre
-  });
+  if (req.body.title !== "" && req.body.releaseYear !== "") {
+    fetch(
+      theMovieDbBaseUrl +
+        `&api_key=${process.env.TMDB_API_KEY}&page=${req.body.page}&query=${
+          req.body.title
+        }&year=${req.body.releaseYear}`
+    )
+      .then(data => {
+        return data.json();
+      })
+      .then(data => {
+        return res.send(
+          JSON.stringify(
+            data.results.filter(movie => {
+              count = 0;
+              for (let i = 0; i < req.body.genre.length; i++) {
+                if (
+                  movie.genre_ids.includes(genreIds[req.body.genre[i]]) ===
+                  false
+                ) {
+                  count++;
+                  i = req.body.genre.length;
+                }
+              }
+              return count === 0;
+            })
+          )
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  } else if (req.body.title !== "") {
+    fetch(
+      theMovieDbBaseUrl +
+        `&api_key=${process.env.TMDB_API_KEY}&page=${req.body.page}&query=${
+          req.body.title
+        }`
+    )
+      .then(data => {
+        return data.json();
+      })
+      .then(data => {
+        return res.send(
+          JSON.stringify(
+            data.results.filter(movie => {
+              return (
+                movie.genre_ids.find(genreIds[req.body.genre]) !== undefined
+              );
+            })
+          )
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  } else if (req.body.releaseYear !== null) {
+    fetch(
+      theMovieDbBaseUrl +
+        `&api_key=${process.env.TMDB_API_KEY}&page=${req.body.page}&year=${
+          req.body.releaseYear
+        }`
+    )
+      .then(data => {
+        return data.json();
+      })
+      .then(data => {
+        return res.send(JSON.stringify(data));
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./routes/search");
+require("dotenv").config();
 
 const app = express();
 
