8467bbe7d979abf6b57a5edf173588803f84c0b8
Stephen <ridingjr@gmail.com>
2019-02-21 14:07:23 -0800

    adjusted search route to filter by genre, year range, and title when a one character title is supplied.

--- routes/search.js
+++ routes/search.js
@@ -6,42 +6,13 @@ const genreIds = require("../movieGenres");
 const theMovieDbBaseUrl =
   "https://api.themoviedb.org/3/search/movie?language=en-US";
 
+//Get request for a movie search using filters such as title, release year, and genre
 router.get("/", (req, res) => {
-  let count = 0;
-
-  if (req.body.title !== "" && req.body.releaseYear !== "") {
-    fetch(
-      theMovieDbBaseUrl +
-        `&api_key=${process.env.TMDB_API_KEY}&page=${req.body.page}&query=${
-          req.body.title
-        }&year=${req.body.releaseYear}`
-    )
-      .then(data => {
-        return data.json();
-      })
-      .then(data => {
-        return res.send(
-          JSON.stringify(
-            data.results.filter(movie => {
-              count = 0;
-              for (let i = 0; i < req.body.genre.length; i++) {
-                if (
-                  movie.genre_ids.includes(genreIds[req.body.genre[i]]) ===
-                  false
-                ) {
-                  count++;
-                  i = req.body.genre.length;
-                }
-              }
-              return count === 0;
-            })
-          )
-        );
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  } else if (req.body.title !== "") {
+  if (
+    req.body.title.length === 1 &&
+    req.body.genre.length > 0 &&
+    req.body.year > 0
+  ) {
     fetch(
       theMovieDbBaseUrl +
         `&api_key=${process.env.TMDB_API_KEY}&page=${req.body.page}&query=${
@@ -52,31 +23,33 @@ router.get("/", (req, res) => {
         return data.json();
       })
       .then(data => {
-        return res.send(
+        let titleRegex = new RegExp("^" + req.body.title + "+", "i");
+        let titleFilteredData = data.results.filter(movie => {
+          return titleRegex.test(movie.title);
+        });
+        let genreFilteredData = titleFilteredData.filter(movie => {
+          let count = 0;
+          for (let i = 0; i < req.body.genre.length; i++) {
+            if (
+              movie.genre_ids.includes(genreIds[req.body.genre[i]]) === false
+            ) {
+              count++;
+              i = req.body.genre.length;
+            }
+          }
+          return count === 0;
+        });
+        let yearRegex = new RegExp(
+          "^" + req.body.year + "[0-9]{2}-[0-9]{2}-[0-9]{2}$",
+          "g"
+        );
+        res.send(
           JSON.stringify(
-            data.results.filter(movie => {
-              return (
-                movie.genre_ids.find(genreIds[req.body.genre]) !== undefined
-              );
+            genreFilteredData.filter(movie => {
+              return yearRegex.test(movie.release_date);
             })
           )
         );
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  } else if (req.body.releaseYear !== null) {
-    fetch(
-      theMovieDbBaseUrl +
-        `&api_key=${process.env.TMDB_API_KEY}&page=${req.body.page}&year=${
-          req.body.releaseYear
-        }`
-    )
-      .then(data => {
-        return data.json();
-      })
-      .then(data => {
-        return res.send(JSON.stringify(data));
       })
       .catch(err => {
         console.log(err);
