b0b6b5289a9c8ab1650cab7de29ad3092a6f4314
Stephen <ridingjr@gmail.com>
2019-05-28 13:03:52 -0700

    some changes to server and package.json for deploying to heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./routes/search");
 const cors = require("cors");
+const path = require("path");
 
 require("dotenv").config();
 
@@ -13,6 +14,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/search", router);
 
-app.listen(5000, (req, res) => {
-  console.log("Did it connect?");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, (req, res) => {});
