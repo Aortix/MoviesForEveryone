0391d3ebadb48b10087780877f1a74187ca9f2c1
Stephen <ridingjr@gmail.com>
2019-06-04 12:17:55 -0700

    trying something...

--- server.js
+++ server.js
@@ -23,12 +23,10 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-
-  app.use(cors(corOptions));
 } else {
   app.use(cors());
 }
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, (req, res) => {});
+app.listen(port, (req, res) => { });
