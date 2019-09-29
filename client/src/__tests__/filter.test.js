dc934ced80ce07b596f9799e11152b615a124f64
Stephen Riding <ridingjr@gmail.com>
2019-09-29 13:20:21 -0700

    setting up jest and enzyme with circleci

---
+++ client/src/__tests__/filter.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Filter from './../components/filter';
+
+import { shallow } from 'enzyme';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Filter />, div);
+});
+
+it('renders without crashing', () => {
+    shallow(<Filter />);
+  });
